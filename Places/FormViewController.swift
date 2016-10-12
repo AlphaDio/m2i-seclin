4e50b71edbb1936a549cb419cff42072158ff3d3
Ludovic OLLAGNIER <ludovic@ollagnier.me>
2016-10-12 09:25:56 +0200

    Added a form view controller

---
+++ Places/FormViewController.swift
@@ -0,0 +1,38 @@
+//
+//  FormViewController.swift
+//  Places
+//
+//  Created by Ludovic Ollagnier on 11/10/2016.
+//  Copyright © 2016 Tec-Tec. All rights reserved.
+//
+
+import UIKit
+
+class FormViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func saveForm(_ sender: AnyObject) {
+        dismiss(animated: true, completion: nil)
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
