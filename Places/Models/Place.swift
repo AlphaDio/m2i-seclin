045bba9b5ed9d0b74274a211961a7cb60fdba843
Ludovic OLLAGNIER <ludovic@ollagnier.me>
2016-10-12 11:01:09 +0200

    Added properties in Place struct

--- Places/Models/Place.swift
+++ Places/Models/Place.swift
@@ -9,5 +9,35 @@
 import Foundation
 
 struct Place {
-    
+
+    enum Source {
+        case local
+        case internet
+
+        // Exemples de méthodes et computed property dans une enum
+        static var allCases: [Source] {
+            return [.local, .internet]
+        }
+
+        func infos() -> (int1: Int, float1: Float, message: String) {
+            switch self {
+            case .local:
+                return (6, 6.5, "Local")
+            case .internet:
+                return (7, 96.5, "Internet")
+            }
+        }
+    }
+
+    let name: String
+    let adress: String
+    let phoneNumber: String?
+    let websiteURL: URL?
+    let note: Float
+    let numberOfReviews: Int
+
+    let latitude: Double
+    let longitude: Double
+
+    let source: Source
 }
