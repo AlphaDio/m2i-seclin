74c594049ffea9c8fe9316ec2cbdbefcc40bc8f8
Ludovic OLLAGNIER <ludovic@ollagnier.me>
2016-10-12 12:10:08 +0200

    Singletonized Directory

--- Places/Models/Directory.swift
+++ Places/Models/Directory.swift
@@ -8,10 +8,16 @@
 
 class Directory {
 
-    private var places: [Place] = []
+    static let instance = Directory()
+
+    private var places: [Place]
 
     var allPlaces: [Place] {
         return places
+    }
+
+    private init() {
+        places = []
     }
 
     func add(_ place: Place) {
