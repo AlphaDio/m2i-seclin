d224d9b73b2b34e59186e64c3f9a87522d07d18a
Ludovic OLLAGNIER <ludovic@ollagnier.me>
2016-10-12 10:04:51 +0200

    Project reorganisation + added model files

---
+++ Places/Models/Directory.swift
@@ -0,0 +1,9 @@
+//
+//  Directory.swift
+//  Places
+//
+//  Created by Ludovic Ollagnier on 12/10/2016.
+//  Copyright © 2016 Tec-Tec. All rights reserved.
+//
+
+import Foundation
---
+++ Places/Models/Place.swift
@@ -0,0 +1,13 @@
+//
+//  Place.swift
+//  Places
+//
+//  Created by Ludovic Ollagnier on 12/10/2016.
+//  Copyright © 2016 Tec-Tec. All rights reserved.
+//
+
+import Foundation
+
+struct Place {
+    
+}
